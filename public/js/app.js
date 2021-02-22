1b60640847f8b7d62e906b15e9d8597768e9d273
unknown <singhshashank32@gmail.com>
2021-02-22 15:52:43 +0530

    Heroku setup updated

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,15 @@
 console.log("Client side javascript file is loaded!");
 
-fetch("http://localhost:3000/weather?address=delhi").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
-  });
-});
+// fetch("http://localhost:3000/weather?address=delhi").then((response) => {
+//   response.json().then((data) => {
+//     if (data.error) {
+//       console.log(data.error);
+//     } else {
+//       console.log(data.location);
+//       console.log(data.forecast);
+//     }
+//   });
+// });
 
 const WeatherForm = document.querySelector("form");
 const locationInput = document.querySelector("input");
